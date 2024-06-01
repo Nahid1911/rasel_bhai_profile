cbbbf46f2d8fb4dadcdb76eaca18cc41da7c786d
Nahid1911 <nahid.raihan@gmail.com>
2024-06-01 13:21:56 +0600

    Add qualifications

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React, { useCallback } from 'react';
+import React, { useCallback } from "react";
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
-import './App.css';
+import "./App.css";
 import particlesOptions from "./particles.json";
-import Header from './components/Header'
-import Intro from './components/Intro';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import ContactForm from './components/ContactForm';
-
+import Header from "./components/Header";
+import Intro from "./components/Intro";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import ContactForm from "./components/ContactForm";
+import Qualifications from "./components/Qualifications";
 
 function App() {
-    const particlesInit = useCallback(main => {
-        loadFull(main);
-    }, [])
+  const particlesInit = useCallback((main) => {
+    loadFull(main);
+  }, []);
 
-    return (
-        <div className="App text-white font-serif">
-            <Particles options={particlesOptions} init={particlesInit}/>
-            <Header />
-            <Intro />
-            <Skills />
-            <Projects />
-            <ContactForm />
-        </div>
-    );
+  return (
+    <div className="App text-white font-serif">
+      <Particles options={particlesOptions} init={particlesInit} />
+      <Header />
+      <Intro />
+      <Qualifications />
+      <Skills />
+      <Projects />
+      <ContactForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Qualifications.js
@@ -0,0 +1,50 @@
+const Qualifications = () => (
+  <section
+    id="Qualifications"
+    className="pt-10 pb-4 lg:pt-22 flex flex-col gap-24"
+  >
+    <div className="flex flex-col flex-1 px-8">
+      <h6 className="text-center font-semibold text-xl sm:text-4xl md:text-5xl">
+        My{" "}
+        <span className="poppins text-violet-400">
+          Associations and Qualifications{" "}
+        </span>
+      </h6>
+    </div>
+    <ol className="text-left pl-4 font-semibold">
+      <li class="py-2 flex flex-row">
+        <i className="fa-solid fa-book text-green-500 mr-2 ml-8" />
+        Associate Member, The Institute of Chartered Accountants of Bangladesh
+        (ICAB)
+      </li>
+      <li class="py-2 flex flex-row">
+        <i className="fa-solid fa-book text-green-500 mr-2 ml-8" />
+        Affiliated Member, The Chartered Institute of Public Finance and
+        Accountancy.
+      </li>
+      <li class="py-2 flex flex-row">
+        <i className="fa-solid fa-book text-green-500 mr-2 ml-8" />
+        Chartered Accountant, The Institute of Chartered Accountants of
+        Bangladesh (ICAB) (2023).
+      </li>
+      <li class="py-2 flex flex-row">
+        <i className="fa-solid fa-book text-green-500 mr-2 ml-8" />
+        CIPFA, the Chartered Institute of Public Finance and Accountancy.
+      </li>
+      <li class="py-2 flex flex-row">
+        <i className="fa-solid fa-book text-green-500 mr-2 ml-8" />
+        Income Tax Practitioner and member of Dhaka Taxes Bar Association.
+      </li>
+      <li class="py-2 flex flex-row">
+        <i className="fa-solid fa-book text-green-500 mr-2 ml-8" />
+        Master of Business Studies, National University (2010).
+      </li>
+      <li class="py-2 flex flex-row">
+        <i className="fa-solid fa-book text-green-500 mr-2 ml-8" />
+        Bachelor of Business Studies, National University (2008).
+      </li>
+    </ol>
+  </section>
+);
+
+export default Qualifications;
